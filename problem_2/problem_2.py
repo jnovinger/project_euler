0f00a367caff6620967607e979d704a2bc72bc74
Jason Novinger <jnovinger@gmail.com>
2011-05-29 20:59:39 -0500

    One of the Project Euler suggested solutions.

--- problem_2/problem_2.py
+++ problem_2/problem_2.py
@@ -4,18 +4,15 @@
 
 limit = 3999999
 
-fib_1 = 1
-fib_2 = 2
+sum = 0
+a = 1
+b = 1
+c = a + b
 
-sum = 2  # preload
-while fib_1 + fib_2 < limit:
-    if (fib_1 + fib_2) % 2 == 0:
-        sum += fib_1 + fib_2
-
-    # update fibs
-    if fib_1 < fib_2:
-        fib_1 = fib_1 + fib_2
-    else:
-        fib_2 = fib_1 + fib_2
+while c < limit:
+    sum += c
+    a = b + c
+    b = c + a
+    c = a + b
 
 print sum
