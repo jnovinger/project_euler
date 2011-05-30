eba594514504ac24ac3cd2b0e024d3967fef34cf
Jason Novinger <jnovinger@gmail.com>
2011-05-29 23:12:08 -0500

    Initial, working!, solution for problem 6.

--- problem_6/problem_6.py
+++ problem_6/problem_6.py
@@ -1,2 +1,19 @@
-__author__ = 'jason'
-  
+#!/usr/bin/python
+
+'''
+ Project Euler Problem 6
+'''
+
+limit = 100
+
+sum_of_squares = 0
+
+sum = 0
+
+for number in xrange(1, limit + 1):
+    sum_of_squares += number ** 2
+    sum += number
+
+square_of_sum = sum ** 2
+
+print square_of_sum - sum_of_squares
