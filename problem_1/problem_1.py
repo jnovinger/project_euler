9ebb60c1994ddb13e5e05ac45e6f39c1dc63b0ed
Jason Novinger <jnovinger@gmail.com>
2011-05-29 19:04:28 -0500

    New algorithm, sum sum of 3 mult's and 5 mult's and subtract 15 mult's

--- problem_1/problem_1.py
+++ problem_1/problem_1.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python
 
+''' Project Euler Problem 1'''
+from math import floor
+
 if __name__ == '__main__':
     sum = 0
-    
-    for number in range(1000):
-        if (number % 3) == 0 or (number % 5) == 0:
-            sum += number
-    
+    target = 999999
+
+    sum3 = 0
+    sum5 = 0
+    sum15 = 0
+
+    index3 = int(floor(target / 3))
+    index5 = int(floor(target / 5))
+    index15 = int(floor(target / 15))
+
+    sum3 = 0
+    sum5 = 0
+    sum15 = 0
+
+    for number in xrange(index3):
+        sum3 += number + 1
+
+    for number in xrange(index5):
+        sum5 += number + 1
+
+    for number in xrange(index15):
+        sum15 += number + 1
+
+    sum = 3 * sum3 + 5 * sum5 - 15 * sum15
+
     print sum
