4cc56a8fac82f01ef5b60278f352d3b58c213833
Jason Novinger <jnovinger@gmail.com>
2011-05-29 19:10:35 -0500

    Suggested Project Euler solution. Wow, that is fast.

--- problem_1/problem_1.py
+++ problem_1/problem_1.py
@@ -1,33 +1,13 @@
 #!/usr/bin/python
 
 ''' Project Euler Problem 1'''
-from math import floor
+
+def SumDivisibleBy(n):
+    p = target / n
+    return n * (p * (p + 1)) / 2
 
 if __name__ == '__main__':
     sum = 0
-    target = 999999
+    target = 999
 
-    sum3 = 0
-    sum5 = 0
-    sum15 = 0
-
-    index3 = int(floor(target / 3))
-    index5 = int(floor(target / 5))
-    index15 = int(floor(target / 15))
-
-    sum3 = 0
-    sum5 = 0
-    sum15 = 0
-
-    for number in xrange(index3):
-        sum3 += number + 1
-
-    for number in xrange(index5):
-        sum5 += number + 1
-
-    for number in xrange(index15):
-        sum15 += number + 1
-
-    sum = 3 * sum3 + 5 * sum5 - 15 * sum15
-
-    print sum
+    print SumDivisibleBy(3) + SumDivisibleBy(5) - SumDivisibleBy(15)
