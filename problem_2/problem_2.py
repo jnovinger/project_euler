76c61f0b5f041a15f4d265cdcc07f1be825002dc
Jason Novinger <jnovinger@gmail.com>
2011-05-29 20:44:57 -0500

    Version without a table. No need to keep track of old Fibonacci's, just the sum.

--- problem_2/problem_2.py
+++ problem_2/problem_2.py
@@ -4,22 +4,18 @@
 
 limit = 3999999
 
-fibs = []
+fib_1 = 1
+fib_2 = 2
 
-fibs.append(1)
-fibs.append(2)
+sum = 2  # preload
+while fib_1 + fib_2 < limit:
+    if (fib_1 + fib_2) % 2 == 0:
+        sum += fib_1 + fib_2
 
-fib_1_index = 0
-fib_2_index = 1
-
-while fibs[fib_1_index] + fibs[fib_2_index] < limit:
-    fibs.append(fibs[fib_1_index] + fibs[fib_2_index])
-    fib_1_index += 1
-    fib_2_index += 1
-
-sum = 0
-for fib in fibs:
-    if (fib % 2 == 0):
-        sum += fib
+    # update fibs
+    if fib_1 < fib_2:
+        fib_1 = fib_1 + fib_2
+    else:
+        fib_2 = fib_1 + fib_2
 
 print sum
