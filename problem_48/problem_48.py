531e24f5f24ea76e168e05c93519bf44c5a0aa4f
Jason Novinger <jnovinger@gmail.com>
2011-06-19 23:01:06 -0500

    Solution for problem 48

--- problem_48/problem_48.py
+++ problem_48/problem_48.py
@@ -6,7 +6,6 @@ Project Euler Problem 48
 The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
 Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
-
 '''
 
 target = 1000
